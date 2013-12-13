7d24bf4b364ac9f4fee8f1c7195a9e1d9225b802
waylon531 <waylon531@gmail.com>
2013-12-13 11:02:56 -0800

    Added script to create table and database

---
+++ initializemySQL.php
@@ -0,0 +1,24 @@
+<?php
+$con=mysqli_connect("localhost","user","password");
+$sql="CREATE DATABASE userpass";
+if (mysqli_query($con,$sql))
+  {
+  echo "Database userpass created successfully<br>";
+  }
+else
+  {
+  echo "Error creating database: " . mysqli_error($con) . "<br>";
+  }
+// Create table
+$sql="CREATE TABLE userpass(username char(64) not null,PRIMARY KEY(username),hash CHAR(255))";
+$con=mysqli_connect("localhost","root","1337haXX0r","userpass");
+// Execute query
+if (mysqli_query($con,$sql))
+  {
+  echo "Table persons created successfully";
+  }
+else
+  {
+  echo "Error creating table: " . mysqli_error($con);
+  }
+?>
