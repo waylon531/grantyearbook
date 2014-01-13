048efd1343233a08ed6d8d9fa96ee0f7e6dfb10f
waylon531 <waylon531@gmail.com>
2014-01-13 15:04:49 -0800

    Fixed initialize mySQL

--- initializemySQL.php
+++ initializemySQL.php
@@ -18,6 +18,16 @@ else
   {
   echo "Error creating database: " . mysqli_error($con) . "<br>";
   }
+$sql="CREATE TABLE files\\ (filename char(64) not null,PRIMARY KEY(filename),uploadedby CHAR(255))";
+$con=mysqli_connect("localhost","user","password","files");
+if (mysqli_query($con,$sql))
+  {
+  echo "Table files created successfully<br>";
+  }
+else
+  {
+  echo "Error creating database: " . mysqli_error($con) . "<br>";
+  }
 // Create table
 $sql="CREATE TABLE userpass(username char(64) not null,PRIMARY KEY(username),hash CHAR(255),realname CHAR(255))";
 $con=mysqli_connect("localhost","user","password","userpass");
