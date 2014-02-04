33cb9a98637197639e48a91f0d9d361f545494bb
coolmanris <rscomputernerd25@gmail.com>
2014-02-03 17:41:07 -0800

    Updated config.ini and initializemySQL.php
    Now initializemySQL.php reads the config.ini so the user can input SQL
    user, password, host, as well as name databases. Currently not
    implemented in any other files, so its set to the default names to
    maintain compatibility.

--- initializemySQL.php
+++ initializemySQL.php
@@ -1,25 +1,26 @@
 <?php
-$con=mysqli_connect("localhost","user","password");
-$sql="CREATE DATABASE userpass";
+$ini_array= parse_ini_file("config.ini");
+$con=mysqli_connect($ini_array['mysqli_connect_HOST'],$ini_array['mysqli_connect_USER'],$ini_array['mysqli_connect_PASS']);
+$sql="CREATE DATABASE " . $ini_array['sql_user_pass_db'];
 if (mysqli_query($con,$sql))
   {
-  echo "Database userpass created successfully<br>";
+  echo "Database " . $ini_array['sql_user_pass_db'] . "created successfully<br>";
   }
 else
   {
   echo "Error creating database: " . mysqli_error($con) . "<br>";
   }
-$sql="CREATE DATABASE files";
+$sql="CREATE DATABASE " . $ini_array['sql_files_db'];
 if (mysqli_query($con,$sql))
   {
-  echo "Database files created successfully<br>";
+  echo "Database " . $ini_array['sql_files_db'] . " created successfully<br>";
   }
 else
   {
   echo "Error creating database: " . mysqli_error($con) . "<br>";
   }
 $sql="CREATE TABLE `files\\` (filename char(64) not null,PRIMARY KEY(filename),uploadedby CHAR(255))";
-$con=mysqli_connect("localhost","user","password","files");
+$con=mysqli_connect($ini_array['mysqli_connect_HOST'],$ini_array['mysqli_connect_USER'],$ini_array['mysqli_connect_PASS'],$ini_array['sql_files_db']);
 if (mysqli_query($con,$sql))
   {
   echo "Table files created successfully<br>";
@@ -30,7 +31,7 @@ else
   }
 // Create table
 $sql="CREATE TABLE userpass(username char(64) not null,PRIMARY KEY(username),hash CHAR(255),realname CHAR(255))";
-$con=mysqli_connect("localhost","user","password","userpass");
+$con=mysqli_connect($ini_array['mysqli_connect_HOST'],$ini_array['mysqli_connect_USER'],$ini_array['mysqli_connect_PASS'],$ini_array['sql_user_pass_db']);
 // Execute query
 if (mysqli_query($con,$sql))
   {
@@ -40,41 +41,5 @@ else
   {
   echo "Error creating table: " . mysqli_error($con) . "<br>";
   }
-/*$con=mysqli_connect("localhost","user","password","files");
-$sql="CREATE TABLE current(filename char(64) not null,PRIMARY KEY(filename),uploadedby CHAR(255))";
-if (mysqli_query($con,$sql))
-  {
-  echo "Table current created successfully<br>";
-  }
-else
-  {
-  echo "Error creating table: " . mysqli_error($con) . "<br>";
-  }
-$sql="CREATE TABLE older(filename char(64) not null,PRIMARY KEY(filename),uploadedby CHAR(255))";
-if (mysqli_query($con,$sql))
-  {
-  echo "Table older created successfully<br>";
-  }
-else
-  {
-  echo "Error creating table: " . mysqli_error($con) . "<br>";
-  }
-$sql="CREATE TABLE oldest(filename char(64) not null,PRIMARY KEY(filename),uploadedby CHAR(255))";
-if (mysqli_query($con,$sql))
-  {
-  echo "Table oldest created successfully<br>";
-  }
-else
-  {
-  echo "Error creating table: " . mysqli_error($con) . "<br>";
-  }
-$sql="CREATE TABLE old(filename char(64) not null,PRIMARY KEY(filename),uploadedby CHAR(255))";
-if (mysqli_query($con,$sql))
-  {
-  echo "Table old created successfully<br>";
-  }
-else
-  {
-  echo "Error creating table: " . mysqli_error($con) . "<br>";
-  }*/
+
 ?>
